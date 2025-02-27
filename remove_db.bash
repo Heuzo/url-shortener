526153d1629d86c2b341f20c56e39db57e15bc74
Heuzo <vlad.neight21@gmail.com>
2025-02-27 11:15:52 +0300

    Initial project version

---
+++ remove_db.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DB_PATH="../storage/storage.db"
+
+if [ -f "$DB_PATH" ]; then
+    if rm "$DB_PATH"; then
+        echo "Файл storage.db удалён."
+    else
+        echo "Ошибка при удалении файла." >&2
+        exit 1
+    fi
+else
+    echo "Файл storage.db не найден."
+fi
