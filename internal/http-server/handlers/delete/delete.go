6845e41adc7e5eadf0b0cf585dafae1b6583aa44
Heuzo <vlad.neight21@gmail.com>
2025-04-02 04:00:44 +0300

    fix conflict

---
+++ internal/http-server/handlers/delete/delete.go
@@ -0,0 +1,38 @@
+package delete
+
+import (
+	"errors"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"log/slog"
+	"net/http"
+	"url-shortener/internal/lib/logger/sl"
+	"url-shortener/internal/storage"
+)
+
+func New(log *slog.Logger, serviceDB storage.SQLService) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		const op = "handlers.url.redirect.Delete"
+		requestId := middleware.GetReqID(request.Context())
+		log = log.With(
+			slog.String("operation", op),
+			slog.String("request_id", requestId),
+		)
+
+		alias := chi.URLParam(request, "alias")
+
+		url, errDB := serviceDB.GetURL(alias)
+		log.Info("Getted alias", "alias", alias)
+		if errors.Is(errDB, storage.ErrURLNotFound) {
+			log.Error("url not found by gien alias", "alias", alias)
+			writer.WriteHeader(http.StatusNotFound)
+		}
+		if errDB != nil {
+			log.Error("failed to get url by given alias:", sl.Err(errDB))
+			writer.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		http.Redirect(writer, request, url, http.StatusFound)
+		log.Info("successfully redirection", slog.String("url", url))
+	}
+}
